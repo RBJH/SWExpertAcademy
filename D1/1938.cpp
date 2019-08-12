d2a5e4f5cc0901b1fdad212446a3d46170803a06
RBJH <46277703+RBJH@users.noreply.github.com>
2019-08-12 15:33:14 +0900

    Solve 1938

---
+++ D1/1938.cpp
@@ -0,0 +1,14 @@
+// 1938. 아주 간단한 계산기
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+int main() {
+  cin >> a >> b;
+
+  cout << a + b << '\n' << a - b << '\n' << a * b << '\n' << a / b << '\n';
+
+  return 0;
+}
