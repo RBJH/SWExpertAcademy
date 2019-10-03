d07f919028fe219b28971d488472f8935581635c
JUNHWAN YUN <j@JUNHWANs-MacBook-Pro.local>
2019-10-03 09:52:16 +0900

    Solve 4522

---
+++ D3/4522. 세상의 모든 팰린드롬.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int T;
+string result;
+
+int main() {
+  cin >> T;
+
+  for (int t = 1; t <= T; t++) {
+    string str;
+
+    cin >> str;
+
+    result = "Exist";
+
+    for (int i = 0; i < str.length() / 2; i++) {
+      if (str[i] != '?' && str[str.length() - 1 - i] != '?' &&
+          str[i] != str[str.length() - 1 - i]) {
+        result = "Not exist";
+        break;
+      }
+    }
+
+    cout << '#' << t << ' ' << result << '\n';
+  }
+
+  return 0;
+}
