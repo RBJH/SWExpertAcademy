e8c8f6b7ec70366def8567f2490d4f8e384b52c8
JUNHWAN YUN <j@JUNHWANs-MacBook-Pro.local>
2019-10-08 21:33:36 +0900

    Solve 5549

---
+++ D3/5549. 홀수일까 짝수일까.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int T;
+string result;
+
+int main() {
+  cin >> T;
+
+  for (int t = 1; t <= T; t++) {
+    string str;
+
+    cin >> str;
+
+    if ((str[str.length() - 1] - '0') % 2)
+      result = "Odd";
+    else
+      result = "Even";
+
+    cout << '#' << t << ' ' << result << '\n';
+  }
+
+  return 0;
+}
